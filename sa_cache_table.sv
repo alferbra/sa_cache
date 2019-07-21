56cb8e21842d8fe3afb08b789bdc56e78ccbe18c
Álvaro <a.fernandezbravo97@gmail.com>
2019-07-21 19:32:14 +0200

    Último commit antes de segmentar

---
+++ sa_cache_table.sv
@@ -0,0 +1,22 @@
+//Cache "look up table"
+
+import cache_definition::cache_table_type;
+import cache_definition::cache_index_type;
+
+module sa_cache_table (
+	input bit clk,
+	input cache_index_type table_index,		//entrie request information: index and we
+	input cache_table_type table_write,		//write port
+	output cache_table_type table_read);	//read port
+	
+	cache_table_type mem [0:1024];	//1024 entries (tag+valid+dirty)
+	
+	always_ff @(posedge clk) begin
+	
+		if (table_index.we)
+			mem [table_index.index] <= table_write;
+		else
+			table_read <= mem [table_index.index];
+	end
+			
+endmodule
